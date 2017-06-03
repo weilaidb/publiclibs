8bab74fe205eef9e2208dc53d53dda9d7fc3009f
Your Name <you@example.com>
2017-06-04 00:05:33 +0800

    1.4 添加cmake编译代码项

---
+++ testlibs/build/CMakeFiles/publiclibs.dir/DependInfo.cmake
@@ -0,0 +1,50 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_C
+  "/home/weilaidb/publiclibs/testlibs/utils/source/alloc.c" "/home/weilaidb/publiclibs/testlibs/build/CMakeFiles/publiclibs.dir/utils/source/alloc.c.o"
+  "/home/weilaidb/publiclibs/testlibs/utils/source/argv_split.c" "/home/weilaidb/publiclibs/testlibs/build/CMakeFiles/publiclibs.dir/utils/source/argv_split.c.o"
+  "/home/weilaidb/publiclibs/testlibs/utils/source/atomic.c" "/home/weilaidb/publiclibs/testlibs/build/CMakeFiles/publiclibs.dir/utils/source/atomic.c.o"
+  "/home/weilaidb/publiclibs/testlibs/utils/source/base64.c" "/home/weilaidb/publiclibs/testlibs/build/CMakeFiles/publiclibs.dir/utils/source/base64.c.o"
+  "/home/weilaidb/publiclibs/testlibs/utils/source/btree.c" "/home/weilaidb/publiclibs/testlibs/build/CMakeFiles/publiclibs.dir/utils/source/btree.c.o"
+  "/home/weilaidb/publiclibs/testlibs/utils/source/chunk.c" "/home/weilaidb/publiclibs/testlibs/build/CMakeFiles/publiclibs.dir/utils/source/chunk.c.o"
+  "/home/weilaidb/publiclibs/testlibs/utils/source/err.c" "/home/weilaidb/publiclibs/testlibs/build/CMakeFiles/publiclibs.dir/utils/source/err.c.o"
+  "/home/weilaidb/publiclibs/testlibs/utils/source/log.c" "/home/weilaidb/publiclibs/testlibs/build/CMakeFiles/publiclibs.dir/utils/source/log.c.o"
+  "/home/weilaidb/publiclibs/testlibs/utils/source/mutex.c" "/home/weilaidb/publiclibs/testlibs/build/CMakeFiles/publiclibs.dir/utils/source/mutex.c.o"
+  "/home/weilaidb/publiclibs/testlibs/utils/source/slab.c" "/home/weilaidb/publiclibs/testlibs/build/CMakeFiles/publiclibs.dir/utils/source/slab.c.o"
+  "/home/weilaidb/publiclibs/testlibs/utils/source/stringops.c" "/home/weilaidb/publiclibs/testlibs/build/CMakeFiles/publiclibs.dir/utils/source/stringops.c.o"
+  "/home/weilaidb/publiclibs/testlibs/utils/source/wire.c" "/home/weilaidb/publiclibs/testlibs/build/CMakeFiles/publiclibs.dir/utils/source/wire.c.o"
+  )
+SET(CMAKE_C_COMPILER_ID "GNU")
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/weilaidb/publiclibs/testlibs/main.cpp" "/home/weilaidb/publiclibs/testlibs/build/CMakeFiles/publiclibs.dir/main.cpp.o"
+  "/home/weilaidb/publiclibs/testlibs/unittest/source/ut_alloc.cpp" "/home/weilaidb/publiclibs/testlibs/build/CMakeFiles/publiclibs.dir/unittest/source/ut_alloc.cpp.o"
+  "/home/weilaidb/publiclibs/testlibs/unittest/source/ut_argv_split.cpp" "/home/weilaidb/publiclibs/testlibs/build/CMakeFiles/publiclibs.dir/unittest/source/ut_argv_split.cpp.o"
+  "/home/weilaidb/publiclibs/testlibs/unittest/source/ut_atomic.cpp" "/home/weilaidb/publiclibs/testlibs/build/CMakeFiles/publiclibs.dir/unittest/source/ut_atomic.cpp.o"
+  "/home/weilaidb/publiclibs/testlibs/unittest/source/ut_base64.cpp" "/home/weilaidb/publiclibs/testlibs/build/CMakeFiles/publiclibs.dir/unittest/source/ut_base64.cpp.o"
+  "/home/weilaidb/publiclibs/testlibs/unittest/source/ut_chunk.cpp" "/home/weilaidb/publiclibs/testlibs/build/CMakeFiles/publiclibs.dir/unittest/source/ut_chunk.cpp.o"
+  "/home/weilaidb/publiclibs/testlibs/unittest/source/ut_list.cpp" "/home/weilaidb/publiclibs/testlibs/build/CMakeFiles/publiclibs.dir/unittest/source/ut_list.cpp.o"
+  "/home/weilaidb/publiclibs/testlibs/unittest/source/ut_log.cpp" "/home/weilaidb/publiclibs/testlibs/build/CMakeFiles/publiclibs.dir/unittest/source/ut_log.cpp.o"
+  "/home/weilaidb/publiclibs/testlibs/unittest/source/ut_mutex.cpp" "/home/weilaidb/publiclibs/testlibs/build/CMakeFiles/publiclibs.dir/unittest/source/ut_mutex.cpp.o"
+  "/home/weilaidb/publiclibs/testlibs/unittest/source/ut_string.cpp" "/home/weilaidb/publiclibs/testlibs/build/CMakeFiles/publiclibs.dir/unittest/source/ut_string.cpp.o"
+  "/home/weilaidb/publiclibs/testlibs/unittest/source/ut_wire.cpp" "/home/weilaidb/publiclibs/testlibs/build/CMakeFiles/publiclibs.dir/unittest/source/ut_wire.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "../include"
+  "../utils/include"
+  "../unittest/include"
+  "../unittest/source"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ testlibs/build/CMakeFiles/publiclibs.dir/cmake_clean.cmake
@@ -0,0 +1,32 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/publiclibs.dir/main.cpp.o"
+  "CMakeFiles/publiclibs.dir/utils/source/slab.c.o"
+  "CMakeFiles/publiclibs.dir/utils/source/alloc.c.o"
+  "CMakeFiles/publiclibs.dir/utils/source/mutex.c.o"
+  "CMakeFiles/publiclibs.dir/utils/source/argv_split.c.o"
+  "CMakeFiles/publiclibs.dir/utils/source/base64.c.o"
+  "CMakeFiles/publiclibs.dir/utils/source/wire.c.o"
+  "CMakeFiles/publiclibs.dir/utils/source/log.c.o"
+  "CMakeFiles/publiclibs.dir/utils/source/stringops.c.o"
+  "CMakeFiles/publiclibs.dir/utils/source/chunk.c.o"
+  "CMakeFiles/publiclibs.dir/utils/source/atomic.c.o"
+  "CMakeFiles/publiclibs.dir/utils/source/btree.c.o"
+  "CMakeFiles/publiclibs.dir/utils/source/err.c.o"
+  "CMakeFiles/publiclibs.dir/unittest/source/ut_alloc.cpp.o"
+  "CMakeFiles/publiclibs.dir/unittest/source/ut_string.cpp.o"
+  "CMakeFiles/publiclibs.dir/unittest/source/ut_chunk.cpp.o"
+  "CMakeFiles/publiclibs.dir/unittest/source/ut_mutex.cpp.o"
+  "CMakeFiles/publiclibs.dir/unittest/source/ut_list.cpp.o"
+  "CMakeFiles/publiclibs.dir/unittest/source/ut_log.cpp.o"
+  "CMakeFiles/publiclibs.dir/unittest/source/ut_wire.cpp.o"
+  "CMakeFiles/publiclibs.dir/unittest/source/ut_base64.cpp.o"
+  "CMakeFiles/publiclibs.dir/unittest/source/ut_atomic.cpp.o"
+  "CMakeFiles/publiclibs.dir/unittest/source/ut_argv_split.cpp.o"
+  "publiclibs.pdb"
+  "publiclibs"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C CXX)
+  INCLUDE(CMakeFiles/publiclibs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
